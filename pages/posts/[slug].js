b295d4cb578043fbca1e9550c7df1dc574e9f2b7
ivanmrsic <imrsic@dickeys.com>
2020-10-06 11:57:17 +0200

    m

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -12,19 +12,26 @@ import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
 import JSON_LD_Script from '../../lib/JSON_LD_Script'
 
-function createPostSchema({ date, coverImage, author, content, title }) {
+
+
+function createPostSchema({ date, coverImage, author, content, title, slug }, router) {
+
+  console.log("slug", slug);
+  console.log("router", router);
   return {
     "@context": "https://schema.org/",
-    "@type": "Blog",
-    datePosted: date,
-    title,
+    "@type": "BlogPosting",
+    url: router.asPath,
+    datePublished: date,
+    headline: title,
+    inLanguage: "en-US",
     image: coverImage,
     description: content.substring(0, 50) + ' ...',
     author: {
       "@type": "Person",
-      name: author.name,
-      image: author.picture
-    }
+      name: author.name
+    },
+    articleBody: content
   }
 }
 
@@ -47,7 +54,7 @@ export default function Post({ post, morePosts, preview }) {
                     {post.title} | Next.js Blog Example with {CMS_NAME}
                   </title>
                   <meta property="og:image" content={post.ogImage.url} />
-                  {JSON_LD_Script(post, createPostSchema)}
+                  {JSON_LD_Script(post, createPostSchema, router)}
 
                 </Head>
                 <PostHeader
